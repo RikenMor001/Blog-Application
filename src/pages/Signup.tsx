40f42f19ec8df67f1b9a3db5905f94004de4b1a7
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-08-23 18:54:04 -0400

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,13 +3,13 @@ import { Quotes } from "../components/Quotes"
 
 export const Signup = () => {
     return <div>
-        <div className="grid grid-cols-2">
-            <div className="invisible lg:visible">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
                 <Auth type={"signup"}/>
             </div>
-            <div className="invisible lg:visible" >
+            <div className="hidden lg:block" >
                 <Quotes/>
-            </div>
+            </div> 
            
         </div>
     </div>
